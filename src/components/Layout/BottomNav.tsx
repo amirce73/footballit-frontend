b547bc5039d87079bcc81ca9b3f907699847df9e
amirce73 <amirmahdice73@gmail.com>
2026-07-04 08:29:04 +0330

    fix navbar buttons active in mobile

--- src/components/Layout/BottomNav.tsx
+++ src/components/Layout/BottomNav.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { useNavigate, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function BottomNav() {
-  const navigate = useNavigate();
   return (
     <nav className="bottom-nav">
-        <button className="bnav-item active" onClick={() => navigate('/dashboard')}><i className="fa fa-home"></i> خانه</button>
-        <button className="bnav-item" onClick={() => navigate('/profile-hub')}><i className="fa fa-user"></i> پروفایل</button>
-        <button className="bnav-item" onClick={() => navigate('/registration')}><i className="fa fa-edit"></i> ثبت‌نام</button>
-        <button className="bnav-item" onClick={() => navigate('/financial-hub')}><i className="fa fa-credit-card"></i> مالی</button>
-        <button className="bnav-item" onClick={() => navigate('/specialized-hub')}><i className="fa fa-building"></i> تخصصی</button>
+        <NavLink to="/dashboard" className={({isActive}) => isActive ? "bnav-item active" : "bnav-item"}><i className="fa fa-home"></i> خانه</NavLink>
+        <NavLink to="/profile-hub" className={({isActive}) => isActive ? "bnav-item active" : "bnav-item"}><i className="fa fa-user"></i> پروفایل</NavLink>
+        <NavLink to="/registration" className={({isActive}) => isActive ? "bnav-item active" : "bnav-item"}><i className="fa fa-edit"></i> ثبت‌نام</NavLink>
+        <NavLink to="/financial-hub" className={({isActive}) => isActive ? "bnav-item active" : "bnav-item"}><i className="fa fa-credit-card"></i> مالی</NavLink>
+        <NavLink to="/specialized-hub" className={({isActive}) => isActive ? "bnav-item active" : "bnav-item"}><i className="fa fa-building"></i> تخصصی</NavLink>
     </nav>
   );
 }
+
